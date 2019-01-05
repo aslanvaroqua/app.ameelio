5624f13cb0de49ae7bac74f2e7b4cc67a7e857d7
Victor Flores <victorm.floresj@gmail.com>
2019-01-04 19:36:02 -0600

    Update LettersReview component to get data state from location prop

--- app/containers/Pages/Letters/Review.js
+++ app/containers/Pages/Letters/Review.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import brand from 'dan-api/dummy/brand';
 import { PapperBlock } from 'dan-components';
@@ -16,6 +17,7 @@ class LettersReview extends React.Component {
   render() {
     const title = brand.name + ' - Review';
     const description = brand.desc;
+    const { location } = this.props;
     return (
       <div>
         <Helmet>
@@ -27,36 +29,46 @@ class LettersReview extends React.Component {
           <meta property="twitter:description" content={description} />
         </Helmet>
         <PapperBlock title="REVIEW" desc="" icon="ios-eye-outline" />
-        <Grid container alignItems="center" spacing={40}>
+        <Grid container spacing={40}>
           <Grid item md={6} xs={12}>
-            <PapperBlock title="Recipient" icon="">
-              <Typography variant="h6" component="h3">JOHN DOE, 12345</Typography>
-              <Typography variant="subtitle2" component="h3">Ulster Correctional Facility</Typography>
-              <Typography variant="subtitle2" component="h3">P.O. Box 800</Typography>
-              <Typography variant="subtitle2" component="h3">Napanoch, New York 123458-0800</Typography>
+            <PapperBlock title="Recipient" desc="" icon="">
+              {location.state && location.state.inmate && (
+                <div>
+                  <Typography variant="h6" component="h3">
+                    {location.state.inmate.name.toUpperCase()}
+                    ,
+                    {location.state.inmate.number}
+                  </Typography>
+                  <Typography variant="subtitle2" component="h3">{location.state.inmate.facility}</Typography>
+                  <Typography variant="subtitle2" component="h3">{location.state.inmate.zip}</Typography>
+                  <Typography variant="subtitle2" component="h3">{location.state.inmate.address}</Typography>
+                </div>
+              )}
             </PapperBlock>
           </Grid>
           <Grid item md={6} xs={12}>
-            <PapperBlock title="Sender" icon="">
+            <PapperBlock title="Sender" desc="" icon="">
               <Typography variant="h6" component="h3">ERICA DOE</Typography>
               <Typography variant="subtitle2" component="h3">46 Worcester Lane</Typography>
               <Typography variant="subtitle2" component="h3">Brooklyn, New York 34568-9683</Typography>
             </PapperBlock>
           </Grid>
           <Grid item md={6} xs={12}>
-            <PapperBlock title="Message" icon="">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie nulla sollicitudin, faucibus dolor eu, semper urna. Pellentesque vitae sodales turpis, nec porttitor ante. Proin egestas gravida semper. Aliquam tristique odio ut erat laoreet malesuada. Nulla hendrerit at nisl eu pretium. Integer ullamcorper elit eget magna malesuada vulputate. In quis vestibulum ante, non hendrerit lorem. Ut malesuada laoreet est, nec gravida nulla molestie eu.
-              Nunc congue eget massa nec aliquam. Donec rutrum lorem sed urna malesuada porta. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum id libero a sapien sollicitudin cursus. Nam erat risus, blandit non elit quis, ornare dignissim lacus. Fusce tempus ex nec magna molestie scelerisque. Mauris at augue vitae orci elementum euismod quis vitae erat. Nullam gravida iaculis ultricies.
-              Integer elementum lectus vel ullamcorper mollis. Suspendisse et vestibulum nisi. Vestibulum quis laoreet ante. Maecenas libero orci, pellentesque et eleifend venenatis, cursus vel est. Ut urna velit, molestie fringilla nisl vel, tristique pharetra sem. Suspendisse eu egestas nunc. Donec erat erat, finibus vel accumsan eget, tincidunt et purus. Nunc sem nisi, faucibus quis leo ut, porttitor condimentum tellus.
+            <PapperBlock title="Message" desc="" icon="">
+              {location.state && location.state.message && (
+                <Typography variant="subtitle1" component="h3">{location.state.message}</Typography>
+              )}
             </PapperBlock>
           </Grid>
           <Grid item md={6} xs={12}>
-            <PapperBlock title="Photo" icon="">
-              <img src={crypto} alt="Letter sender" />
+            <PapperBlock title="Photo" desc="" icon="">
+              {location.state && location.state.images && (
+                <img src={location.state.images.length === 0 ? crypto : location.state.images[0]} alt="Letter sender" />
+              )}
             </PapperBlock>
           </Grid>
         </Grid>
-        <Button color="primary" variant="contained" onClick={this.clickHandler}>
+        <Button color="primary" variant="contained" onClick={this.clickHandler} style={{ marginBottom: 40 }}>
           Send Letter
         </Button>
       </div>
@@ -64,4 +76,7 @@ class LettersReview extends React.Component {
   }
 }
 
+LettersReview.propTypes = {
+  location: PropTypes.object.isRequired,
+};
 export default LettersReview;
